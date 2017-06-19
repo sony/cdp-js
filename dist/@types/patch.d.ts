8dae8917af3e9ecd756ac5f49f2a55119ca17e07
Shin-Ogata <Shin.Ogata@jp.sony.com>
2017-06-19 17:22:56 +0900

    updated: patch.d.ts

--- dist/@types/patch.d.ts
+++ dist/@types/patch.d.ts
@@ -2,59 +2,28 @@
  * @cdp/mobile patch.d.ts 
  */
 
-interface IScroll {
-}
-
-declare module "iscroll" {
-    let _IScroll: IScroll;
-    export { _IScroll as IScroll };
-}
+///////////////////////////////////////////////////////////////////////
+// hogan.js:
 
 declare module "hogan" {
-    let _Hogan: any;
+    const _Hogan: any;
     export { _Hogan as Hogan };
 }
 
-/*
-// jquerymobile
-interface PopupOptions {
-    // [CDP modified]: add inteface.
-    x?: number | string;
-    y?: number | string;
-    dismissible?: boolean;
-    create?: (event: JQueryEventObject, ui: any) => any;
-    afterclose?: (event: JQueryEventObject, ui: any) => any;
+///////////////////////////////////////////////////////////////////////
+// iscroll.js:
+
+interface IScroll {}
+declare module "iscroll" {
+    const _IScroll: IScroll;
+    export { _IScroll as IScroll };
 }
 
-interface ChangePageOptions {
-    // [CDP modified]: add inteface.
-    fromHashChange?: boolean;
-}
+///////////////////////////////////////////////////////////////////////
+// flipsnap.js:
 
-interface JQueryMobilePath {
-    // [CDP modified]: add inteface.
-    isPath(url: string): boolean;
-    convertUrlToDataUrl(url: string): string;
-    documentBase: any;
+interface IFlipsnap {}
+declare module "flipsnap" {
+    const _Flipsnap: IFlipsnap;
+    export { _Flipsnap as Flipsnap };
 }
-
-interface JQueryMobile extends JQueryMobileOptions {
-    // [CDP modified]: add inteface.
-    back(): void;
-    navigate: any;
-}
-
-interface JQuery {
-    // [CDP modified]: add inteface.
-    page(command?: any, ...args: any[]): any;
-    jqmData(key: string): any;
-    jqmData(key: string, value: any): JQuery;
-    jqmRemoveData(key: string): JQuery;
-    one(events: string, handler: (eventObject: JQueryEventObject, data: any) => any): JQuery;
-}
-
-interface JQueryStatic {
-    // [CDP modified]: add inteface.
-    migrateMute: boolean;   // jquery-migrate
-}
-*/
