bf3b5e6ae13cfbe4a93c2326c1c939551745c561
Shin-Ogata <Shin.Ogata@jp.sony.com>
2017-06-21 21:22:26 +0900

    added: master-task package
    - 共通タスク管理のための private repository

---
+++ packages/master-tasks/provider.js
@@ -0,0 +1,40 @@
+﻿/* eslint-env node, es6 */
+'use strict';
+const fs    = require('fs');
+const path  = require('path');
+
+let task_dir;
+let required_tasks;
+
+try {
+    const config = require(path.join(process.cwd(), 'project.config'));
+    required_tasks = config.required_tasks;
+    if (!required_tasks) {
+        throw Error('no task required.');
+    }
+    task_dir = config.dir.task;
+    if (!task_dir) {
+        throw Error('task directory not defined.');
+    }
+} catch (error) {
+    console.warn(error);
+    process.exit(0);
+}
+
+const dstTaskDir = path.join(process.cwd(), task_dir);
+if (!fs.existsSync(dstTaskDir)) {
+    // create tasks dir
+    fs.mkdirSync(dstTaskDir);
+}
+
+required_tasks.forEach((task) => {
+    const src = path.join(__dirname, 'tasks', task);
+    const dst = path.join(dstTaskDir, task);
+    if (fs.existsSync(src)) {
+        // overwrite
+        fs.writeFileSync(dst, fs.readFileSync(src).toString(), 'utf8');
+    } else {
+        console.error('task not found: ' + task);
+        process.exit(1);
+    }
+});
---
+++ packages/master-tasks/tasks/command.js
@@ -0,0 +1,50 @@
+﻿/* eslint-env node, es6 */
+'use strict';
+const path  = require('path');
+const spawn = require('child_process').spawn;
+
+function exec(command, args, options) {
+    if (!(args instanceof Array)) {
+        args = args.trim().split(' ');
+    }
+    return new Promise((resolve, reject) => {
+        const opt = Object.assign({}, {
+            stdio: 'inherit',
+            stdout: (data) => { /* noop */ },
+            stderr: (data) => { /* noop */ },
+        }, options);
+
+        let resolveCmd;
+        if ('npm' === command) {
+            resolveCmd = 'npm' + (process.platform === 'win32' ? '.cmd' : '');
+        } else {
+            resolveCmd =
+                path.join(__dirname, '..', 'node_modules/.bin', command) +
+                (process.platform === 'win32' ? '.cmd' : '');
+        }
+        const child = spawn(resolveCmd, args, opt)
+            .on('error', (msg) => {
+                reject(msg);
+            })
+            .on('close', (code) => {
+                if (0 !== code) {
+                    reject('error occered. code: ' + code);
+                } else {
+                    resolve(code);
+                }
+            });
+
+        if ('pipe' === opt.stdio) {
+            child.stdout.on('data', (data) => {
+                opt.stdout(data.toString());
+            });
+            child.stderr.on('data', (data) => {
+                opt.stderr(data.toString());
+            });
+        }
+    });
+}
+
+module.exports = {
+    exec: exec,
+};
