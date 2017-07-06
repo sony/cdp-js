63acf35c3e49c7678f192742b6e69da40b744124
Shin-Ogata <Shin.Ogata@jp.sony.com>
2017-07-06 14:02:35 +0900

    added: nativebridge.ts
    - コミット漏れ対応

---
+++ packages/cdp-nativebridge/exports/cdp/bridge/nativebridge.ts
@@ -0,0 +1,11 @@
+﻿import _bridge = require("cdp.nativebridge");
+
+// @class Gate
+export type  Gate = CDP.NativeBridge.Gate;
+export const Gate = _bridge.Gate;
+
+// interfaces
+export type Feature             = CDP.NativeBridge.Feature;
+export type ConstructOptions    = CDP.NativeBridge.ConstructOptions;
+export type IResult             = CDP.NativeBridge.IResult;
+export type ExecOptions         = CDP.NativeBridge.ExecOptions;
