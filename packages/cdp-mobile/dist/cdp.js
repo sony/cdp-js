7fd6da15fc7f987a7d092c05f09304423b4779b4
Shin-Ogata <Shin.Ogata@jp.sony.com>
2017-07-14 16:40:34 +0900

    fixed: header ui-controlgroup-horizontal adjust
    - 公式のヘッダのグループボタン定義に対応
      http://demos.jquerymobile.com/1.4.5/toolbar/
    　

--- packages/cdp-mobile/dist/cdp.js
+++ packages/cdp-mobile/dist/cdp.js
@@ -1,7 +1,7 @@
 /*!
  * @cdp/mobile 2.0.0
  *
- * Date: 2017-07-13T09:10:32.332Z
+ * Date: 2017-07-14T07:36:35.237Z
  *
  * including:
  *     @cdp/core 2.0.0
@@ -26273,7 +26273,7 @@ return CDP.UI; }));
 /*!
  * cdp.ui.jqm.js 2.0.0
  *
- * Date: 2017-07-13T06:17:48.026Z
+ * Date: 2017-07-14T07:36:17.038Z
  */
 (function (root, factory) { if (typeof define === "function" && define.amd) { define("cdp.ui.jqm",["cdp.framework.jqm", "cdp.tools", "cdp.ui.listview"], function () { return factory(root.CDP || (root.CDP = {})); }); } else { factory(root.CDP || (root.CDP = {})); } }(this, function (CDP) { CDP.UI = CDP.UI || {};
 var CDP;
