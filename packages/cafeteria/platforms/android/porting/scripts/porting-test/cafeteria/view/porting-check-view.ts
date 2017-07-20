a6ce319734fcc3034d6bcec32fb22c41fb113b46
Shin-Ogata <Shin.Ogata@jp.sony.com>
2017-07-20 18:20:23 +0900

    added: cafeteria android porting-test module

---
+++ packages/cafeteria/platforms/android/porting/scripts/porting-test/cafeteria/view/porting-check-view.ts
@@ -0,0 +1,41 @@
+﻿import {
+    registerPage,
+    PageView,
+    ShowEventData,
+    Theme,
+} from "cdp/ui";
+
+import Func from "../model/porting-function";
+
+/**
+ * @class PortingTestView
+ * @brief Porting 確認用ビュークラス
+ */
+class PortingTestView extends PageView implements IPortingPageView {
+    /**
+     * constructor
+     */
+    constructor() {
+        super("/porting/templates/porting-test.html",
+            "porting-test",
+            {
+                route: "advanced/porting"
+            }
+        );
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    // Override: PageView
+
+    onPageBeforeShow(event: JQuery.Event, data?: ShowEventData): void {
+        super.onPageBeforeShow(event, data);
+        Theme.setCurrentUIPlatform("android");
+    }
+
+    onPageShow(event: JQuery.Event, data: ShowEventData): void {
+        super.onPageShow(event, data);
+        Func.sayHello();
+    }
+}
+
+registerPage(PortingTestView);
