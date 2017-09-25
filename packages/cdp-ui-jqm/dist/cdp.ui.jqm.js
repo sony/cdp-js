458f6ccd10514240a892ef12c27ff96fa4fd5961
Shin-Ogata <Shin.Ogata@jp.sony.com>
2017-09-25 12:04:15 +0900

    updated: dependency modules updated: .gitigunore

--- packages/cdp-ui-jqm/dist/cdp.ui.jqm.js
+++ packages/cdp-ui-jqm/dist/cdp.ui.jqm.js
@@ -1,7 +1,7 @@
 ﻿/*!
  * cdp.ui.jqm.js 2.0.0
  *
- * Date: 2017-09-06T09:23:23.657Z
+ * Date: 2017-09-25T02:49:25.133Z
  */
 (function (root, factory) { if (typeof define === "function" && define.amd) { define(["cdp.framework.jqm", "cdp.tools", "cdp.ui.listview"], function () { return factory(root.CDP || (root.CDP = {})); }); } else { factory(root.CDP || (root.CDP = {})); } }(this, function (CDP) { CDP.UI = CDP.UI || {};
 var CDP;
--- packages/cdp-ui-listview/dist/cdp.ui.listview.js
+++ packages/cdp-ui-listview/dist/cdp.ui.listview.js
@@ -1,7 +1,7 @@
 ﻿/*!
  * cdp.ui.listview.js 2.0.0
  *
- * Date: 2017-09-06T09:21:53.396Z
+ * Date: 2017-09-25T02:47:42.273Z
  */
 (function (root, factory) { if (typeof define === "function" && define.amd) { define(["jquery", "underscore", "backbone"], function ($, _, Backbone) { return factory(root.CDP || (root.CDP = {}), $, _, Backbone); }); } else if (typeof exports === "object") { module.exports = factory(root.CDP || (root.CDP = {}), require("jquery"), require("underscore"), require("backbone")); } else { factory(root.CDP || (root.CDP = {}), root.$, root._, root.Backbone); } }(((this || 0).self || global), function (CDP, $, _, Backbone) { CDP.UI = CDP.UI || {};
 /// <reference types="jquery" />
